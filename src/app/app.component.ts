08413c141807bd855d37b209b8d5922010c27871
Gustavo Esteven Fajardo Cruz <gfajardo1997@gmail.com>
2022-05-07 00:58:52 -0500

    inicio proyecto gifApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  // template:`
-  // <h1>Hola de nuevo desde bacticks</h1>
-  // `
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
