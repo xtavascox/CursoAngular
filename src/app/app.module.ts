eac3581cf72108ebba648cb36bdd331402741e0a
Gustavo Esteven Fajardo Cruz <gfajardo1997@gmail.com>
2022-05-08 02:52:54 -0500

    inicio app paises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { GifsModule } from './gifs/gifs.module';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, SharedModule, GifsModule, HttpClientModule],
+    imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent],
 })
