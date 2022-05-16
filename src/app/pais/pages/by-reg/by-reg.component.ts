4beb8ad189bbd79c919aa4913ca32a10b50c7e7a
Gustavo Esteven Fajardo Cruz <gfajardo1997@gmail.com>
2022-05-15 21:43:47 -0500

    finalziacion de seccion 9

--- src/app/pais/pages/by-reg/by-reg.component.ts
+++ src/app/pais/pages/by-reg/by-reg.component.ts
@@ -1,16 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { RESTCountryResponse } from '../../interfaces/pais.interface';
 
 @Component({
-  selector: 'app-by-reg',
-  templateUrl: './by-reg.component.html',
-  styles: [
-  ]
+    selector: 'app-by-reg',
+    templateUrl: './by-reg.component.html',
+    styles: [
+        `
+            button {
+                margin-right: 5px;
+            }
+        `,
+    ],
 })
-export class ByRegComponent implements OnInit {
+export class ByRegComponent {
+    regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+    activeRegion: string = '';
+    countries: RESTCountryResponse[] = [];
+    isLoad: boolean = false;
+    constructor(private regionService: PaisService) {}
 
-  constructor() { }
+    getActiveClass(region: string): string {
+        return region === this.activeRegion
+            ? 'btn btn-primary'
+            : 'btn btn-outline-primary';
+    }
 
-  ngOnInit(): void {
-  }
+    activarRegion(region: string) {
+        if (region === this.activeRegion) {
+            return;
+        }
 
+        this.isLoad = true;
+        this.activeRegion = region;
+        this.countries = [];
+        this.regionService.findRegion(region).subscribe({
+            next: (resp) => {
+                this.countries = resp;
+                this.isLoad = false;
+            },
+        });
+    }
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RESTCountryResponse } from '../interfaces/pais.interface';
@@ -9,17 +9,34 @@ import { RESTCountryResponse } from '../interfaces/pais.interface';
 export class PaisService {
     private _apiUrl: string = 'https://restcountries.com/v3.1/';
 
+    get getParams() {
+        return new HttpParams().set(
+            'fields',
+            'name,flags,population,idd,capital,cca2'
+        );
+    }
+
     constructor(private http: HttpClient) {}
 
     findCountry(query: string): Observable<RESTCountryResponse[]> {
         const url = `${this._apiUrl}/name/${query}`;
 
-        return this.http.get<RESTCountryResponse[]>(url);
+        return this.http.get<RESTCountryResponse[]>(url, {
+            params: this.getParams,
+        });
     }
 
     findCapital(query: string): Observable<RESTCountryResponse[]> {
-        const url = `${this._apiUrl}/capital/${query}`;
-        return this.http.get<RESTCountryResponse[]>(url);
+        const url = `${this._apiUrl}/capital/${query}?fields=name,flags,population,idd,capital,cca2`;
+        return this.http.get<RESTCountryResponse[]>(url, {
+            params: this.getParams,
+        });
+    }
+    findRegion(query: string): Observable<RESTCountryResponse[]> {
+        const url = `${this._apiUrl}/region/${query}?fields=name,flags,population,idd,capital,cca2`;
+        return this.http.get<RESTCountryResponse[]>(url, {
+            params: this.getParams,
+        });
     }
     findId(query: string): Observable<RESTCountryResponse[]> {
         const url = `${this._apiUrl}/alpha/${query}`;
